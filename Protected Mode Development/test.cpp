5f5864d83eea6b473ebd5a745c8934a4a78ee6d7
KingRayuga <yashraj1605@gmail.com>
2023-12-26 23:09:58 +0530

    project setup

---
+++ Protected Mode Development/test.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    cout<<"Project setup for protected mode "<<endl;
+}
---
+++ Real Mode Development/test.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    cout<<"project setup for real mode dev"<<endl;
+}
