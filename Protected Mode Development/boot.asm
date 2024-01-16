c255b0d0c9bee05227361607a06c6fd11a91e999
KingRayuga <yashraj1605@gmail.com>
2024-01-17 02:09:00 +0530

    add: pmode boot-loader

---
+++ Protected Mode Development/boot.asm
@@ -0,0 +1,73 @@
+ORG 0x7C00
+BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
+
+start:
+    jmp 0:step2
+
+step2:
+    cli
+    mov ax, 0x00
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+
+.load_protected:
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+gdt_code:
+    dw 0xffff
+    dw 0
+    db 0
+    db 0x9a
+    db 11001111b
+    db 0
+
+gdt_data:
+    dw 0xffff
+    dw 0
+    db 0
+    db 0x92
+    db 11001111b
+    db 0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+    jmp $
+
+times 510-($ - $$) db 0
+dw 0xAA55
--- Protected Mode Development/test.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(void){
-    cout<<"Project setup for protected mode "<<endl;
-}
