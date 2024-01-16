082aaa555142b225141656d65db546f8af7f911a
KingRayuga <yashraj1605@gmail.com>
2024-01-17 02:58:34 +0530

    update: kernel, linker

--- Protected Mode Development/kernel.asm
+++ Protected Mode Development/kernel.asm
@@ -3,7 +3,6 @@ global _start
 
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
-
 _start:
     mov ax, DATA_SEG
     mov ds, ax
@@ -13,10 +12,11 @@ _start:
     mov ss, ax
     mov ebp, 0x00200000
     mov esp, ebp
-
     ; Enable the A20 line
     in al, 0x92
     or al, 2
     out 0x92, al
 
     jmp $
+
+times 512-($ - $$) db 0
